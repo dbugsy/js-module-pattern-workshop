9712688b953e6a6e946a7760f0018105e490127c
ddosque <dpbergquist@gmail.com>
2017-05-23 09:36:46 +0100

    remove src files

--- test/exclaim-spec.js
+++ test/exclaim-spec.js
@@ -2,8 +2,4 @@ describe("exclaim", function() {
   it("should add an exclamation mark to passed string", function() {
     expect(exclaim("hello")).toEqual("hello!");
   });
-
-  it("should add an exclamation mark to passed string", function() {
-    expect(exclaim("yo")).toEqual("yo!");
-  });
 });
--- test/question-spec.js
+++ test/question-spec.js
@@ -1,6 +1,5 @@
 describe("question", function() {
   it("should add two question marks to passed string", function() {
-    question = new Question("hello")
-    expect(question.toString()).toEqual("hello??");
+    expect(question("hello")).toEqual("hello??");
   });
 });
